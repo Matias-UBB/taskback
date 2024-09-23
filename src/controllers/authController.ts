6a0d3fa66fb539298bae46568079fc39fb8ffc79
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2024-09-23 20:58:19 -0300

    Refactor tokenService and authService

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,10 @@
-import { Request, Response, NextFunction } from "express";
+import  { Request, Response, NextFunction } from "express";
 import * as userService from "../service/authService";
 import { validate } from "class-validator";
 import { plainToClass } from "class-transformer";
 import { CreateUserDto, LoginUserDto } from "../dto/userDto";
 import { AppError } from "../middleware/errorHandler";
+import { generateAccesToken, verifyRefreshToken } from "../jwt/tokenService";
 
 
 
@@ -16,7 +17,7 @@ const register = async (req: Request, res : Response, next:NextFunction):Promise
             return;
         }
         const newUser = await userService.createUser(userDto);
-        res.status(201).json({});
+        res.status(201).json({message: "User created successfully", user: newUser});
     } catch (error) {
         next(error);
 
@@ -24,28 +25,53 @@ const register = async (req: Request, res : Response, next:NextFunction):Promise
 };
 
 const login = async (req: Request, res : Response, next: NextFunction):Promise<void>=>{
-    const loginDto= plainToClass(LoginUserDto,req.body);
-    const errors = await validate(loginDto);
-    if(errors.length>0){
-        res.status(400).json({errors});
-        return;
-    }
+   
 
-    console.log("login");
+   
     try {
+        const loginDto= plainToClass(LoginUserDto,req.body);
+        const errors = await validate(loginDto);
+        if(errors.length>0){
+             res.status(400).json({errors});
+             return;
+        }
+
         const user = await userService.loginUser(loginDto);
         if(!user){
             res.status(401).json({message:"Invalid credentials"});
             return;
         }
-        res.status(200).json(user);
+        res.status(200).json({
+            message:"Login successful",
+            accessToken:user.accessToken,
+            refreshToken:user.refreshToken,
+            user:{email:user.email,name:user.name}
+        });
     } catch (error) {
-        next(error);
+       return  next(error);
+    }
+};
+
+const refreshToken = async (req: Request, res: Response, next: NextFunction):Promise<void>=>{
+    const { refreshToken } = req.body;
+    if(!refreshToken){
+         return next(new AppError("Refresh token is required",400));
+    };
+
+
+    try {
+        const payload= verifyRefreshToken(refreshToken);
+        const newAccessToken = generateAccesToken({userId:payload.userId,email:payload.email});
+        res.status(200).json({ accessToken: newAccessToken });
+       
+    } catch (error) {
+        return next(new AppError("Invalid token",401));
     }
 };
 
 export {
     register,
-    login
+    login,
+    refreshToken
 };
 
